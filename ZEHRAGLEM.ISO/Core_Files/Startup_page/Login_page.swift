aee4376b01f84568c6e40b02cd959c57c737ffd4
mohammed islam <mohammedislam1115@gmail.com>
2023-06-08 17:20:15 -0400

    Zeheraglem iso app project added

---
+++ ZEHRAGLEM.ISO/Core_Files/Startup_page/Login_page.swift
@@ -0,0 +1,20 @@
+//
+//  Login_page.swift
+//  ZEHRAGLEM.ISO
+//
+//  Created by Mohammed Islam on 6/8/23.
+//
+
+import SwiftUI
+
+struct Login_page: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Login_page_Previews: PreviewProvider {
+    static var previews: some View {
+        Login_page()
+    }
+}
---
+++ ZEHRAGLEM.ISO/ZEHRAGLEM_ISOApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZEHRAGLEM_ISOApp.swift
+//  ZEHRAGLEM.ISO
+//
+//  Created by Mohammed Islam on 6/8/23.
+//
+
+import SwiftUI
+
+@main
+struct ZEHRAGLEM_ISOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
